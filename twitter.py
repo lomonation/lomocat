7ddc3cbfabed78fb7856a30165041256c1dc8af5
maxdeviant <mad.mod.max@gmail.com>
2014-04-07 15:39:05 -0400

    Working Twitter auth.

---
+++ twitter.py
@@ -0,0 +1,8 @@
+import tweepy
+from config import *
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+# api.update_status('First run!')
