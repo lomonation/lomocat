525cb13853cf8f8879c9ed1f029545d3dc91dd93
maxdeviant <mad.mod.max@gmail.com>
2014-04-08 13:05:52 -0400

    Added server status check
    Although the python-nmap library seems to be very slow.

--- lomocat.py
+++ lomocat.py
@@ -1,24 +1,36 @@
-import argparse, subprocess
+import argparse, subprocess, nmap
+from config import SERVER_IP, SERVER_PORT
 from twitter import *
 from personality import *
 
 class LomoCat(object):
     def __init__(self):
+        self.nm = nmap.PortScanner()
         self.twitter = Twitter()
 
     def tweet(self, message):
         print self.twitter.post(message)
+
+    def status(self):
+        self.nm.scan(SERVER_IP, SERVER_PORT)
+        return self.nm[SERVER_IP]['tcp'][int(SERVER_PORT)]['state']
 
     def mc_command(self, command):
         return subprocess.call(['screen', '-S', 'minecraft', '-X', 'stuff', '%s\015' % command])
 
     def minecraft(self, command):
         if (command.lower() == 'start'):
-            print 'Starting server.'
-            self.mc_command('java -Xmx1024M -Xms1024M -jar minecraft_server.1.7.5.jar nogui')
+            if (self.status() == 'open'):
+                print 'Server already running.'
+            else:
+                print 'Starting server.'
+                self.mc_command('java -Xmx1024M -Xms1024M -jar minecraft_server.1.7.5.jar nogui')
         elif (command.lower() == 'stop'):
-            print 'Stopping server.'
-            self.mc_command('stop')
+            if (self.status() == 'closed'):
+                print 'No server instance found.'
+            else:
+                print 'Stopping server.'
+                self.mc_command('stop')
         else:
             print 'Command not recognized.'
 
@@ -39,6 +51,8 @@ def main():
 
     parser = argparse.ArgumentParser(description='a bot for Lomonation')
 
+    parser.add_argument('-s', '--status', action='store_true', help='check server status')
+
     parser.add_argument('-t', '--tweet', metavar='message', type=str, help='post a tweet')
 
     parser.add_argument('-m', '--minecraft', metavar='command', type=str, help='issue a Minecraft server command')
@@ -48,6 +62,9 @@ def main():
     parser.add_argument('-c', '--cartograph', metavar='command', type=str, help='render server map')
 
     args = parser.parse_args()
+
+    if (args.status):
+        print cat.status().capitalize()
 
     if (args.tweet is not None):
         cat.tweet(args.tweet)
